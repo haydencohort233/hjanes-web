83020cc2495d094702ad22bf2ed36fe53fb599ba
haydencohort233 <hayden.janes.cohort233@gmail.com>
2024-12-05 23:46:55 -0800

    Removed redundacy

--- src/components/DesktopGrid.js
+++ src/components/DesktopGrid.js
@@ -4,17 +4,11 @@ import { useDroppable } from '@dnd-kit/core';
 import styles from './DesktopGrid.module.css';
 
 const DesktopGrid = ({ id }) => {
-  const { isOver, setNodeRef } = useDroppable({
+  const { setNodeRef } = useDroppable({
     id: id,
   });
 
-  // Inline style for visibility during development (can be removed later)
-  const style = {
-    width: '100%',
-    height: '100%'
-  };
-
-  return <div ref={setNodeRef} style={style} className={styles.gridCell}></div>;
+  return <div ref={setNodeRef} className={styles.gridCell}></div>;
 };
 
 export default DesktopGrid;
